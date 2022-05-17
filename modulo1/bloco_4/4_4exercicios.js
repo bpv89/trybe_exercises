4968489ac86093c67e138449a84e559534a0c41a
Bruno Veiga <eubrunoveiga@gmail.com>
2022-05-17 12:55:32 -0300

    ex 4 feito

--- modulo1/bloco_4/4_4exercicios.js
+++ modulo1/bloco_4/4_4exercicios.js
@@ -25,3 +25,22 @@ for (let index in info) {
 for (let index in info) {
     console.log(info[index]);
 }
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+for (let index in info) {
+    if (index == 'recorrente'){
+        if (info[index] == info2[index]) {
+            console.log('Ambos recorrentes');
+        } 
+        }else {
+            console.log( info[index] + ' e ' + info2[index]);
+    }
+}
