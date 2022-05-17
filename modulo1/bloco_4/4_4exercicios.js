3fa43d35235ebaa7cdcb9d784046169fcd00b7ee
Bruno Veiga <eubrunoveiga@gmail.com>
2022-05-17 13:06:45 -0300

    ex 6 e 7 feitos

--- modulo1/bloco_4/4_4exercicios.js
+++ modulo1/bloco_4/4_4exercicios.js
@@ -45,3 +45,31 @@ for (let index in info) {
     }
 }
 
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+//6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log('O livro favorito de ' + leitor.nome + " " + leitor.sobrenome + " se chama '" + 
+leitor['livrosFavoritos'][0]['titulo'] + "'" );
+
+// 🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
+leitor.livrosFavoritos[1] = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  }
+
