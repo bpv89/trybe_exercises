864a564f9f3b16bc6243c7f19d755bcc53983ba5
Bruno Veiga <eubrunoveiga@gmail.com>
2022-06-23 14:50:44 -0300

    termino da aula

--- modulo1/bloco_8/aula_2/index.js
+++ modulo1/bloco_8/aula_2/index.js
@@ -46,4 +46,50 @@ const musicas = [
     return musica.title;
   }
   
-  console.log(findMusic('31031685'))
+  console.log(findMusic('31031685'));
+
+//   1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
+
+const nomes = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((nome) => nome === name);
+}
+
+console.log(hasName(nomes, 'Ana'))
+
+// 2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false, use every;
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, minimumAge) => {
+    return arr.every((item) => item.age >= minimumAge)
+  }
+  
+  console.log(verifyAges(people, 16));
+
+//   1 - Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente.
+
+const peoples = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  peoples.sort((a,b) => a.age > b.age ? 1:-1); 
+  
+  console.log(peoples);
+
+//   2 - Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+  
+peoples.sort((a,b) => a.age < b.age ? 1:-1); 
+  
+console.log(peoples);
