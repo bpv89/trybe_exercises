7c5d572cd08d0f02165beb3b339ae5bd42f95761
Bruno Veiga <eubrunoveiga@gmail.com>
2022-07-04 10:22:27 -0300

    aula 1 do frontEnd

---
+++ modulo2/bloco_9/aula_1/callback.js
@@ -0,0 +1,21 @@
+// 1 - Adicione uma callback como parâmetro da função getUser.
+// No código abaixo você tem a função getUser, que define um objeto com os dados de uma pessoa. Complete a função getUser de forma que ela receba uma função callback como parâmetro para realizar as operações abaixo:
+// Insira o retorno da função getUser;
+// Complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich";
+// Complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian".
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (parm) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return parm(userToReturn);
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
